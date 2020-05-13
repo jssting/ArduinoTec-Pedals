2288363c8f9eabc0631f674e14e23bec6e2b2437
jssting <55811693+jssting@users.noreply.github.com>
2020-05-13 13:30:16 +1000

    Added a config file
    Added a config file to enable debugging and set deadzone without needing to change code in the INO file

---
+++ ArduinoTec Pedals/confOptions.h
@@ -0,0 +1,16 @@
+//confOptions.h
+//debug options
+#define Enable_Debug false
+#define Debug_Thr true
+#define Debug_Cth false
+#define Debug_Brk false
+
+// initial Deadzone
+#define brake_L_DZ 5
+#define throttle_L_DZ 5
+#define clutch_L_DZ 5
+
+// maximum Deadzone
+#define brake_U_DZ 5
+#define throttle_U_DZ 5
+#define clutch_U_DZ 5
